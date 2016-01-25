a439257eae675426cdca54da0ac09888c20795c3
Pmagnard <pascal.magnard@laposte.net>
2016-01-25 17:05:54 +0100

    Multi results page test mode (no access to a search image service, the results are hand made to test the result page )

--- app/src/main/java/pmag/snapandsearch/search/SearchActivity.java
+++ app/src/main/java/pmag/snapandsearch/search/SearchActivity.java
@@ -1,13 +1,11 @@
 package pmag.snapandsearch.search;
 
 import java.io.File;
-import java.io.StringReader;
+import java.security.AccessController;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
+import java.util.ListIterator;
 
 import pmag.snapandsearch.SnapAndSearchAbstractActivity;
 import pmag.snapandsearch.SnapAndSearchInterface;
@@ -16,132 +14,90 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 public class SearchActivity extends SnapAndSearchAbstractActivity implements SnapAndSearchInterface {
 	/* List of XML tags to ignore in the result of the search */
-	private final static List<String> XMLTAGS_TO_IGNORE = Arrays.asList("ARQueryMatches", "ProductMatches", "BarcodeMatches", "Thumbnail", "ProductId", "SearchQuery");
+	/* available mode are : Test, JustVisual, GoogleVision, BingVisual */
+	private static String switchMode = "Test";
+
+
 	
-	private class SearchTimer extends AsyncTask<String, Void, String> {
-		private String imageFileName;
-		
+	private class SearchTimer extends AsyncTask<String, Void, List<SearchResult>> {
+		private String searchedImageFileName;
+		private SearchImageHelper imageSearcher = new SearchImageHelper(true, MY_APP_NAME);
+
 		@Override
-		protected String doInBackground(String... param) {
+		protected List<SearchResult> doInBackground(String... param) {
 			// search by image
-			SearchImageHelper imageSearcher = new SearchImageHelper(true, MY_APP_NAME);
-			imageFileName = param[0];
-			return imageSearcher.searchBingByImage(new File(imageFileName));
+			searchedImageFileName = param[0];
+			List<SearchResult> listSearchResult = null;
+			switch (switchMode) {
+				case "Test":
+					listSearchResult = imageSearcher.searchTestByImage(new File(searchedImageFileName));
+					// no real search
+				break;
+				case "BingVisual":
+					listSearchResult = imageSearcher.searchBingByImage(new File(searchedImageFileName));
+					break;
+			}
+			return listSearchResult;
 		}
-		private boolean isTagToBeDisplayed(String tagName) {
-			
-			return ((tagName!=null) &&(!XMLTAGS_TO_IGNORE.contains(tagName)));
-		}
+
 		@Override
-		protected void onPostExecute(String searchResult) {
+		protected void onPostExecute(List<SearchResult> searchResults) {
 
-			
-			String parsedSearchResult = null;
-			if (searchResult != null) {
-				Log.i(MY_APP_NAME, searchResult );
-				parsedSearchResult= parseXMLResult(searchResult);
-			}
-
-		
-			//parsing the resulting XML 
-			if ((searchResult != null) && (searchResult.length() > 0)) {
-				if ((parsedSearchResult == null) || (parsedSearchResult.length()== 0)) {
-					parsedSearchResult = "NO MATCH";
-				}
-			}
-			else {
-				parsedSearchResult = "NO INTERNET CONNECTION";
-			}
-			//displaying the result
-			TextView textView = (TextView) findViewById(R.id.text_result_id);
-			//textView.setMovementMethod(new ScrollingMovementMethod()); // make the text area scrollable
-			textView.setText(parsedSearchResult);
-					
-
-			
+			ListIterator<SearchResult> results = searchResults.listIterator();
+			SearchResult result= null;
 			// dismiss the progress circle
 			ProgressBar progressCycle = (ProgressBar) findViewById(R.id.progress_bar_id);
 			progressCycle.setVisibility(View.GONE);
-			
-			//set the image in the image view
-			ImageView imageView = (ImageView) findViewById(R.id.image_view_id);
-			imageView.setImageDrawable(Drawable.createFromPath(imageFileName));
-			
+
+			if (results != null) {
+				LinearLayout verticalLayout = (LinearLayout) findViewById(R.id.linear_layout_id);
+
+				while (results.hasNext()) {
+					result = results.next();
+					LinearLayout layout = new LinearLayout(getApplicationContext());
+					layout.setOrientation(LinearLayout.VERTICAL);
+					// set the text components
+					ImageView imageView = new ImageView(getApplicationContext());
+					imageView.setImageDrawable(Drawable.createFromPath(result.getImage().getPath()));
+					layout.addView(imageView);
+
+					TextView text = new TextView(getApplicationContext());
+					text.setText(result.getComment());
+					layout.addView(text);
+
+					text = new TextView(getApplicationContext());
+					text.setText(result.getUrl());
+					text.setLinksClickable(true);
+					layout.addView(text);
+
+					text = new TextView(getApplicationContext());
+					text.setText(result.getImageName());
+					layout.addView(text);
+
+					verticalLayout.addView(layout);
+				}
+
+			}
 			//remove the file now
-			File delFile = new File(imageFileName);
+			File delFile = new File(searchedImageFileName);
 			Log.d(MY_APP_NAME, "File size is " + delFile.length() + " bytes");
 			if (!delFile.delete()) {
 				Log.i(MY_APP_NAME, "Image file couldn't be deleted");
 			}
 			
 		}
-		
-		private String parseXMLResult(String searchResult) {
-			StringBuffer textBuffer = new StringBuffer();
-	 		String tagName=null;
 
-			XmlPullParserFactory xmlParserFactory;
-			XmlPullParser xmlParser;
-			try {
-				xmlParserFactory = XmlPullParserFactory.newInstance();
-				xmlParserFactory.setNamespaceAware(true);
-		 		xmlParser = xmlParserFactory.newPullParser();
-		        xmlParser.setInput(new StringReader(searchResult));
-			} catch (XmlPullParserException e) {
-				Log.e(MY_APP_NAME, "error parsing XML file", e);
- 				return null;
-			}
-		
-	 		
-	 		int eventType;
-	 		try {
-	 			eventType = xmlParser.getEventType();
-	 		} catch (XmlPullParserException e) {
-	 			Log.e(MY_APP_NAME, "error parsing XML file", e);
-	 			return null;
-	 		}
-	 		while (eventType != XmlPullParser.END_DOCUMENT) {
-	 			if (eventType == XmlPullParser.START_DOCUMENT) {
-	 				Log.i(MY_APP_NAME, "Start document");
-	 			} else if (eventType == XmlPullParser.START_TAG) {
-	 				tagName = xmlParser.getName();
-	 				Log.i(MY_APP_NAME, "Start tag " + tagName);
-	 				if (isTagToBeDisplayed(tagName)) {
-	 					textBuffer.append("** " + tagName);
-	 					textBuffer.append(System.getProperty("line.separator"));
-	 					//show attributes
-	 					for (int n=0; n < xmlParser.getAttributeCount(); n++) {
-	 						textBuffer.append(xmlParser.getAttributeName(n) + ": ");
-	 						textBuffer.append(xmlParser.getAttributeValue(n)+ System.getProperty("line.separator"));
-	 					}
-	 				}
-	 			} else if (eventType == XmlPullParser.END_TAG) {
-	 				Log.i(MY_APP_NAME, "End tag " + xmlParser.getName());
-	 			} else if (eventType == XmlPullParser.TEXT) {
-	 				Log.i(MY_APP_NAME, "Text " + xmlParser.getText());
-	 				if (isTagToBeDisplayed(tagName)) {
-	 					textBuffer.append(xmlParser.getText());
-	 					textBuffer.append(System.getProperty("line.separator"));
-	 				}
-	 			}
-	 			try {
-	 				eventType = xmlParser.next();
-	 			} catch (Exception e) {
-	 				Log.e(MY_APP_NAME, "error parsing XML file", e);
-	 				return null;
-	 			}
-	 		}
-	 		Log.i(MY_APP_NAME, "End document");
-	 		return textBuffer.toString();
-		}
-		
 	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/pmag/snapandsearch/search/SearchImageHelper.java
+++ app/src/main/java/pmag/snapandsearch/search/SearchImageHelper.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -18,11 +22,16 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.params.HttpParams;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.util.Log;
 
-public class SearchImageHelper {
+import pmag.snapandsearch.SnapAndSearchInterface;
 
+public class SearchImageHelper {
+	private final static List<String> XMLTAGS_TO_IGNORE = Arrays.asList("ARQueryMatches", "ProductMatches", "BarcodeMatches", "Thumbnail", "ProductId", "SearchQuery");
 	private String logAppName = "SearchImageHelper";
 
 	/* set debug mode in Android mode or System.out mode */
@@ -47,6 +56,10 @@ public class SearchImageHelper {
 			System.err.println(logId + " " + logMessage + t);
 		}
 	}
+	private boolean isTagToBeDisplayed(String tagName) {
+
+		return ((tagName!=null) &&(!XMLTAGS_TO_IGNORE.contains(tagName)));
+	}
 
 	private HttpResponse executeHttpRequest(HttpClient httpClient, HttpRequestBase request) {
 		HttpResponse response = null;
@@ -59,16 +72,54 @@ public class SearchImageHelper {
 		return response;
 	}
 
+	/**
+	 * Create a test search result
+	 * @param imageFile
+	 * @return a list of fake results
+	 */
+	public List<SearchResult> searchTestByImage(File imageFile) {
+		List<SearchResult> searchResults = new ArrayList();
+		// first result
+		SearchResult searchResult = new SearchResult();
+		searchResult.setComment("Image 1");
+		searchResult.setImage(imageFile);
+		searchResult.setImageName(imageFile.getName());
+		searchResult.setUrl("http://www.google.fr");
+		searchResults.add(searchResult);
+		// second result
+		searchResult = new SearchResult();
+		searchResult.setComment("Image 2");
+		searchResult.setImage(imageFile);
+		searchResult.setImageName(imageFile.getName());
+		searchResult.setUrl("http://www.google.com");
+		searchResults.add(searchResult);
+		// third result
+		searchResult = new SearchResult();
+		searchResult.setComment("Image 3");
+		searchResult.setImage(imageFile);
+		searchResult.setImageName(imageFile.getName());
+		searchResult.setUrl("http://www.google.com");
+		searchResults.add(searchResult);
+		// fourth result
+		searchResult = new SearchResult();
+		searchResult.setComment("Image 4");
+		searchResult.setImage(imageFile);
+		searchResult.setImageName(imageFile.getName());
+		searchResult.setUrl("http://www.google.com");
+		searchResults.add(searchResult);
+		return searchResults;
+
+	}
 
 	/**
 	 * search by image using Microsoft Bing service
-	 * 
+	 *
 	 * @param imageFile
 	 */
-	public String searchBingByImage(File imageFile) {
+	public List<SearchResult> searchBingByImage(File imageFile) {
 		// HTTP Client with some parameters
 		HttpParams httpParams = new BasicHttpParams();
-		// set HTTP timeout 
+		// set HTTP timeout
 		httpParams.setIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, HTTP_TIMEOUT);
 		HttpClient httpClient = new DefaultHttpClient();
 		httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
@@ -102,13 +153,11 @@ public class SearchImageHelper {
 				}
 			}
 		}
-		return responseBody.toString();
+		return parseXMLResult(responseBody.toString());
 	}
-
 	private HttpResponse uploadImageToBing(File imageFile, HttpClient httpClient) throws IOException {
 
 		{
-
 			HttpPost httpPost = new HttpPost("http://wp.bingvision.ar.glbdns.microsoft.com/ImageSearchV2.ashx");
 			FileInputStream fis = new FileInputStream(imageFile);
 			byte[] imageBytes = new byte[(int) imageFile.length()];
@@ -123,4 +172,69 @@ public class SearchImageHelper {
 		}
 
 	}
+
+	private List<SearchResult> parseXMLResult(String searchStringResult) {
+        List<SearchResult> searchResults = new ArrayList<>();
+        StringBuffer textBuffer = new StringBuffer();
+         String tagName=null;
+
+        XmlPullParserFactory xmlParserFactory;
+        XmlPullParser xmlParser;
+        try {
+            xmlParserFactory = XmlPullParserFactory.newInstance();
+            xmlParserFactory.setNamespaceAware(true);
+             xmlParser = xmlParserFactory.newPullParser();
+            xmlParser.setInput(new StringReader(searchStringResult));
+        } catch (XmlPullParserException e) {
+            Log.e(SnapAndSearchInterface.MY_APP_NAME, "error parsing XML file", e);
+             return null;
+        }
+
+
+         int eventType;
+         try {
+             eventType = xmlParser.getEventType();
+         } catch (XmlPullParserException e) {
+             Log.e(SnapAndSearchInterface.MY_APP_NAME, "error parsing XML file", e);
+             return null;
+         }
+         while (eventType != XmlPullParser.END_DOCUMENT) {
+             if (eventType == XmlPullParser.START_DOCUMENT) {
+                 Log.i(SnapAndSearchInterface.MY_APP_NAME, "Start document");
+             } else if (eventType == XmlPullParser.START_TAG) {
+                 tagName = xmlParser.getName();
+                 Log.i(SnapAndSearchInterface.MY_APP_NAME, "Start tag " + tagName);
+                 if (isTagToBeDisplayed(tagName)) {
+                    SearchResult searchResult = new SearchResult();
+
+                     textBuffer.append("** " + tagName);
+                     textBuffer.append(System.getProperty("line.separator"));
+
+                     //show attributes
+                     for (int n=0; n < xmlParser.getAttributeCount(); n++) {
+                         textBuffer.append(xmlParser.getAttributeName(n) + ": ");
+                         textBuffer.append(xmlParser.getAttributeValue(n)+ System.getProperty("line.separator"));
+                     }
+                    searchResult.setComment(textBuffer.toString());
+                    searchResults.add(searchResult);
+                 }
+             } else if (eventType == XmlPullParser.END_TAG) {
+                 Log.i(SnapAndSearchInterface.MY_APP_NAME, "End tag " + xmlParser.getName());
+             } else if (eventType == XmlPullParser.TEXT) {
+                 Log.i(SnapAndSearchInterface.MY_APP_NAME, "Text " + xmlParser.getText());
+                 if (isTagToBeDisplayed(tagName)) {
+                     textBuffer.append(xmlParser.getText());
+                     textBuffer.append(System.getProperty("line.separator"));
+                 }
+             }
+             try {
+                 eventType = xmlParser.next();
+             } catch (Exception e) {
+                 Log.e(SnapAndSearchInterface.MY_APP_NAME, "error parsing XML file", e);
+                 return null;
+             }
+         }
+         Log.i(SnapAndSearchInterface.MY_APP_NAME, "End document");
+         return searchResults;
+    }
 }
